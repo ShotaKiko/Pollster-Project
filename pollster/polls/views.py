fec50a9b580350214cbb50ed73184d7d6f0ffaf3
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-16 22:12:28 -0600

    created index html and template view for polls/using bootstrap for styling

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Question,Choice
+
+#Get question and displays
+
+def index(request):
+    return render(request, 'polls/index.html')
