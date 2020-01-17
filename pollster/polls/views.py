05fe7917ce75fcca5c709a259c44178ae0df1aca
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-16 23:29:26 -0600

    templates for details and results

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -17,7 +17,7 @@ def detail(request, question_id):
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 #get que and display vote results
 def results(request, question_id):
