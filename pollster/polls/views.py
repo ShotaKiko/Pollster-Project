03a1b857c83acaf1e701bde33fd1f8bbda3443e1
Shota Kiko <Shota.Kikozashvili@gmail.com>
2020-01-16 23:21:51 -0600

    added views for details and results-->templates to follow

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -10,3 +10,16 @@ def index(request):
     latest_question_list = Question.objects.all()
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+#show specidic que and choice
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', {'question': question})
+
+#get que and display vote results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question })
